c4f6024171760b5569f694c06be069fdd5ede951
PANKAJ BHATT <211pankajbhatt@gmail.com>
2024-02-25 21:29:09 +0530

    added current time of user search

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const APIkey = 'c670a7547db47b192f9197a34cd1fbfb';
 
 function handleit() {
   locationinput = document.getElementById('locationInput').value;
-
+  
+  
   const lonlet = `http://api.openweathermap.org/geo/1.0/direct?q=${locationinput}&appid=${APIkey}`;
 
   fetch(lonlet)
@@ -17,6 +18,9 @@ function handleit() {
 
       tem(latitudevalue, longitudevalue);
       timetemprature(latitudevalue, longitudevalue,APIkey);
+      
+      
+      
     })
     .catch(error => console.error('Error:', error));
 
@@ -31,9 +35,33 @@ function handleit() {
         const descriptionmain = tempdata.weather[0].main;
         const description = tempdata.weather[0].description;
         updateTemperature(temperatureincel, descriptionmain, description);
+
+        
+        
       })
       .catch(error => console.error('Error:', error));
   }
+
+
+  function realtime() {
+    const currenttimeUrl = `https://timezone.abstractapi.com/v1/current_time?api_key=4d041e26cd2643c9ba34606173e9f063&location=${locationinput} `;
+  
+    fetch(currenttimeUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        const curtim =  data.datetime;
+        updateCurrentTime(curtim);
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+      });
+  }
+  
 
 
 
@@ -46,12 +74,15 @@ function handleit() {
         // Clear previous forecast data
         document.getElementById('timeforecast-container').innerHTML = '';
   
+        
         for (const forecast of timedata.list) {
           const dateTime = forecast.dt_txt;
           const temperature = Math.round(forecast.main.temp);
-  
-          updatetimetemp(dateTime, temperature);
+          
+            updatetimetemp(dateTime, temperature);
         }
+
+        
       })
       .catch(error => console.error('Error:', error));
   }
@@ -76,6 +107,7 @@ function handleit() {
     temperaturePlaceholder.textContent = `${temperature}°C`;
     container.classList.add('transition-effect');
   }
+  realtime(locationinput);
 
   function updatetimetemp(dateTime, temperature) {
   const timeforecastContainer = document.getElementById('timeforecast-container');
@@ -86,12 +118,18 @@ function handleit() {
 
   // Format the date and time
   const formattedDateTime = new Date(dateTime);
-  const timeString = formattedDateTime.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
-
-
+  const timeString = formattedDateTime.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });    
   timebox.innerHTML = `<p>${timeString}</p><br><p>${temperature}°C</p>`;
 
  
   timeforecastContainer.appendChild(timebox);
 }
+
+function updateCurrentTime(curtim){
+  const ate= new Date(curtim)
+  document.getElementById("datehai").innerHTML=ate.toLocaleTimeString('en-US');
+  document.getElementById("pnak").innerHTML=ate.toLocaleDateString();
+  setInterval(realtime(),1000)
+}
+
 }
